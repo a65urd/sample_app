0db8552885ed652ac8ed2a9dc79f10a5f0c38213
Alex LP <alexlp86@gmail.com>
2013-01-14 16:42:28 +0400

    chapter_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  match '/signup',  :to => 'users#new'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
+
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
