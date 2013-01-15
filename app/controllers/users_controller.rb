54de8363194ab01ea260276b2875b768c9fe58d9
Alex LP <alexlp86@gmail.com>
2013-01-15 17:05:42 +0400

    chapter_6.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-  def new
-  	@title="Sign Up"
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
   end
+
+  def new
+  	@user = User.find(params[:id])
+    @title = @user.name
+  end
+
 end
